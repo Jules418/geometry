e593fa4a577cca2ffbe65fbe89e56e4dffdeef39
Jules418 <julio34310@gmail.com>
2024-04-18 18:56:57 +0200

    goes to gcc instead of cc (temp)

--- geometry.h
+++ geometry.h
@@ -6,18 +6,18 @@
 /*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 21:40:26 by jules             #+#    #+#             */
-/*   Updated: 2024/04/17 16:15:54 by jules            ###   ########.fr       */
+/*   Updated: 2024/04/17 17:18:41 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GEOMETRY_H
 # define GEOMETRY_H
 
-# include "vector.h"
-# include "ray.h"
-# include "sphere.h"
-# include "plane.h"
-# include "cylinder.h"
+# include "vector/vector.h"
+# include "includes/ray.h"
+# include "includes/sphere.h"
+# include "includes/plane.h"
+# include "includes/cylinder.h"
 
 # define EPSILON 0.0001f
 
